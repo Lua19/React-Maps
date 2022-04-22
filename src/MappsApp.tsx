4d89a766caf8dfa0fd703f1104705536f1fb509f
Lua19 <miguellua100@gmail.com>
2022-04-22 10:25:27 -0600

    Added places provider

---
+++ src/MappsApp.tsx
@@ -0,0 +1,9 @@
+import { PlacesProvider } from "./context"
+
+export const MapsApp = () => {
+    return (
+        <PlacesProvider>
+            <h1>Hola Mundo otra vez</h1>
+        </PlacesProvider>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { MapsApp } from './MappsApp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <MapsApp/>
   </React.StrictMode>,
   document.getElementById("root")
 )
