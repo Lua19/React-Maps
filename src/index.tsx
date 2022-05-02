1b410d2d3e73cbcec2d49e7cdd77cf4f56e42c66
Lua19 <miguellua100@gmail.com>
2022-05-02 15:25:03 -0600

    Added mapbox

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MapsApp } from './MappsApp';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiaXZhbmx1YTEwMCIsImEiOiJjbDJmYjJla3cwNGFjM2NvY294eDcwa3JxIn0.aJ_XC7SB-0YVsaaW-vWuHA';
+
 
 if (!navigator.geolocation) {
   alert("Your browser doesnt have acces to Geo-Location")
