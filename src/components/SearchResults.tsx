f729319be3c115648fb7f3521ad094d59c6b56f8
Lua19 <miguellua100@gmail.com>
2022-05-06 17:12:07 -0600

    Added select location

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { MapContext, PlacesContext } from "../context"
 import { Feature } from "../interfaces/places";
 import { LoadingPlaces } from "./";
@@ -7,6 +7,7 @@ export const SearchResults = () => {
 
   const {places, isLoadingPlaces} = useContext(PlacesContext)
   const {map} = useContext(MapContext)
+  const [activeId, setActiveId] = useState('')
 
   if (isLoadingPlaces) {
       return( 
@@ -17,6 +18,7 @@ export const SearchResults = () => {
     return <></>;
   }
   const travel = (place: Feature) =>{
+    setActiveId(place.id)
     const [lng, lat] = place.center
         map?.flyTo({
             zoom: 16,
@@ -30,11 +32,11 @@ export const SearchResults = () => {
       {places.map( place => (
         <li 
         key={place.id} 
-        className='list-group-item list-group-item-action pointer' 
+        className={`list-group-item list-group-item-action pointer ${ (activeId === place.id)? 'chosen': ''} ` }
         onClick={() => travel(place)} >
 
           <h6>{place.text_en}</h6>
-          <p className="text-muted" style={{fontSize: '12px'}}>
+          <p style={{fontSize: '12px'}}>
             {place.place_name}
           </p>
           <button className="btn btn-outline-warning btn-sm" >
